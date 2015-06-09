4c029d634197a45f704f8002099999d0572e66a5
Ashley L Hill <ashley.hill@homes.com>
2015-06-09 17:25:22 -0400

    Karma watching

--- tasks/test.js
+++ tasks/test.js
@@ -1,16 +1,71 @@
-var gulp = require('gulp'),
-karma = require('karma').server,
-path = require('path');
-
-
-module.exports = function (gulp) {
 /**
- * Run test once and exit
+ * testing tasks (using karma to test in the browser). Requires a karma.conf.js for full config
+ * single-run testing
+ * continuous testing
  */
-gulp.task('test', function (done) {
-  karma.start({
-    configFile: __dirname + '/../config/karma.conf.js',
-    singleRun: true
-  }, done);
-});
+
+/** base deps, but you may need more specifically for your application */
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var path = require('path');
+var karma = require('karma');
+var karmaParseConfig = require('karma/lib/config').parseConfig;
+
+function runKarma(configFilePath, options, cb) {
+
+  configFilePath = path.resolve(configFilePath);
+
+  var server = karma.server;
+  var log=gutil.log, colors=gutil.colors;
+  var config = karmaParseConfig(configFilePath, {});
+
+    Object.keys(options).forEach(function(key) {
+      config[key] = options[key];
+    });
+
+  server.start(config, function(exitCode) {
+    log('Karma has exited with ' + colors.red(exitCode));
+    cb();
+    process.exit(exitCode);
+  });
 }
+
+/** actual tasks */
+module.exports = function (gulp) {
+
+/** single run */
+gulp.task('test', function(cb) {
+  runKarma('./config/karma.conf.js', {
+    autoWatch: false,
+    singleRun: true
+  }, cb);
+});
+
+/** continuous ... using karma to watch (feel free to circumvent that;) */
+gulp.task('test-dev', function(cb) {
+  runKarma('./config/karma.conf.js', {
+    autoWatch: true,
+    singleRun: false
+  }, cb);
+});
+
+};
+
+
+
+// var gulp = require('gulp'),
+// karma = require('karma').server,
+// path = require('path');
+
+
+// module.exports = function (gulp) {
+// *
+//  * Run test once and exit
+
+// gulp.task('test', function (done) {
+//   karma.start({
+//     configFile: __dirname + '/../config/karma.conf.js',
+//     singleRun: true
+//   }, done);
+// });
+// }
